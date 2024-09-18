3dbe69e55d36e6450a1c2dc5e28450ccbed7ebfe
sillygod <sillygod@livemail.tw>
2024-09-18 06:48:30 +0000

    update by ci

---
+++ _next/static/chunks/app/about/page-5f5a4c35f6b022d9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,64],{5327:function(e,n,s){Promise.resolve().then(s.bind(s,212)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[309,971,23,744],function(){return e(e.s=5327)}),_N_E=e.O()}]);
